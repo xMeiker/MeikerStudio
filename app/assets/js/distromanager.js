043766eed8cce255bd7be22352eaa0f6ba68d802
xMeiker <89752513+xMeiker@users.noreply.github.com>
2023-10-12 19:04:44 -0700

    Update Distro

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -5,7 +5,7 @@ const ConfigManager = require('./configmanager')
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
 // exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://meikerstudio.000webhostapp.com/nebula/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://meikerstudio.000webhostapp.com/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
